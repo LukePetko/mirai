82b9ca8abcee2ee0637a5d9cf08b8b0fa86c7d09
Lukáš Peťko <luke.petko@gmail.com>
2025-11-18 18:49:49 +0100

    adding loading mechanism for automations

--- lib/mirai/AutomationEngine.ex
+++ lib/mirai/AutomationEngine.ex
@@ -11,11 +11,39 @@ defmodule Mirai.AutomationEngine do
   end
 
   def init(_) do
-    {:ok, %{}}
+    automations = discover_automations()
+    Logger.info("Discovered #{length(automations)} automations: #{inspect(automations)}")
+    {:ok, %{automations: automations}}
   end
 
   def handle_cast({:trigger, event}, state) do
     Logger.info("Triggered event: #{inspect(event)}")
+
+    Enum.each(state.automations, fn automation ->
+      GenServer.cast(automation, {:check_event, event, self()})
+    end)
+
     {:noreply, state}
+  end
+
+  defp discover_automations do
+    automations_path = Path.join(:code.priv_dir(:mirai), "automations")
+
+    case File.ls(automations_path) do
+      {:ok, files} ->
+        files
+        |> Enum.filter(&String.ends_with?(&1, ".ex"))
+        |> Enum.map(fn file ->
+          file_path = Path.join(automations_path, file)
+          Code.compile_file(file_path)
+
+          module_name = file |> String.replace(".ex", "") |> Macro.camelize()
+          String.to_atom(module_name)
+        end)
+
+      {:error, _} ->
+        Logger.warning("No automations found in #{automations_path}")
+        []
+    end
   end
 end
--- lib/mirai/application.ex
+++ lib/mirai/application.ex
@@ -7,6 +7,8 @@ defmodule Mirai.Application do
 
   @impl true
   def start(_type, _args) do
+    automations = discover_and_compile_automations()
+
     children = [
       {Mirai.HA.Connector,
        host: System.get_env("HA_HOST", "homeassistant.local"),
@@ -15,11 +17,28 @@ defmodule Mirai.Application do
       {Mirai.AutomationEngine, []}
       # Starts a worker by calling: Mirai.Worker.start_link(arg)
       # {Mirai.Worker, arg}
-    ]
+    ] ++ Enum.map(automations, fn automation -> {automation, []} end)
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Mirai.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp discover_and_compile_automations do
+    automations_path = Path.join(:code.priv_dir(:mirai), "automations")
+    
+    case File.ls(automations_path) do
+      {:ok, files} ->
+        files
+        |> Enum.filter(&String.ends_with?(&1, ".ex"))
+        |> Enum.flat_map(fn file ->
+          file_path = Path.join(automations_path, file)
+          Code.compile_file(file_path)
+          |> Enum.map(fn {mod, _} -> mod end)
+        end)
+      
+      {:error, _} -> []
+    end
   end
 end
